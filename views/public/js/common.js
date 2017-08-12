d6f79289dc04e6d3889df03f5b6616ab55995b2e
pipixia <pipixia@qq.com>
2017-08-12 19:52:40 +0800

    记录登录状态/限制页面跳转到登录页面

--- views/public/js/common.js
+++ views/public/js/common.js
@@ -1,3 +1,4 @@
+/*
 
 	NProgress.start();
 
@@ -5,4 +6,10 @@
 
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
-	});
+	});*/
+
+
+//验证是否有登录(有登录的话服务器会返回一个secctionId到浏览器的),没有登录的话(并且不是login页面的话)跳转到登录页面
+	if(!$.cookie('PHPSECCID')&&location.pathname.indexOf('/login')==-1){
+		window.location.href = '/login';
+	}
